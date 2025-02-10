322cfd363022d46a6ee1134c7818e6d63b6f8df8
FoundDream <2979058069@qq.com>
2025-02-10 11:06:51 +0800

    feat: 处理跨域问题

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // 配置 CORS，允许 localhost 访问
+  app.enableCors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
